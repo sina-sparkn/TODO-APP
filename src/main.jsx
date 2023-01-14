5c8bff24085a637a4d4f5b0a1ed791472b9089e2
ssparknt <sina.hakim.red@gmail.com>
2023-01-14 23:05:44 +0200

    removed!

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
-import "../dist/output.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
